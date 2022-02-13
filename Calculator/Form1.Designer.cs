e28999423445e578c0d3a2eca3ddd9041bd440ff
shabialili <sebnem.eliyeva1998@gmail.com>
2022-02-13 22:00:48 +0400

    Anket

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿
+namespace Calculator
+{
+    partial class Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tb_surname = new System.Windows.Forms.TextBox();
+            this.tb_father = new System.Windows.Forms.TextBox();
+            this.tb_country = new System.Windows.Forms.TextBox();
+            this.tb_city = new System.Windows.Forms.TextBox();
+            this.tb_number = new System.Windows.Forms.TextBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.Female = new System.Windows.Forms.RadioButton();
+            this.Male = new System.Windows.Forms.RadioButton();
+            this.lbl_gender = new System.Windows.Forms.Label();
+            this.lbl_born = new System.Windows.Forms.Label();
+            this.lbl_number = new System.Windows.Forms.Label();
+            this.lbl_city = new System.Windows.Forms.Label();
+            this.lbl_country = new System.Windows.Forms.Label();
+            this.lbl_father = new System.Windows.Forms.Label();
+            this.lbl_surname = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.tb_filename = new System.Windows.Forms.TextBox();
+            this.Load = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.tb_surname);
+            this.groupBox1.Controls.Add(this.tb_father);
+            this.groupBox1.Controls.Add(this.tb_country);
+            this.groupBox1.Controls.Add(this.tb_city);
+            this.groupBox1.Controls.Add(this.tb_number);
+            this.groupBox1.Controls.Add(this.tb_name);
+            this.groupBox1.Controls.Add(this.Female);
+            this.groupBox1.Controls.Add(this.Male);
+            this.groupBox1.Controls.Add(this.lbl_gender);
+            this.groupBox1.Controls.Add(this.lbl_born);
+            this.groupBox1.Controls.Add(this.lbl_number);
+            this.groupBox1.Controls.Add(this.lbl_city);
+            this.groupBox1.Controls.Add(this.lbl_country);
+            this.groupBox1.Controls.Add(this.lbl_father);
+            this.groupBox1.Controls.Add(this.lbl_surname);
+            this.groupBox1.Controls.Add(this.lbl_name);
+            this.groupBox1.Controls.Add(this.Save);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 557);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Form";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Checked = false;
+            this.dateTimePicker1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(174, 356);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(260, 34);
+            this.dateTimePicker1.TabIndex = 18;
+            // 
+            // tb_surname
+            // 
+            this.tb_surname.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_surname.Location = new System.Drawing.Point(145, 81);
+            this.tb_surname.Name = "tb_surname";
+            this.tb_surname.Size = new System.Drawing.Size(289, 34);
+            this.tb_surname.TabIndex = 17;
+            // 
+            // tb_father
+            // 
+            this.tb_father.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_father.Location = new System.Drawing.Point(145, 121);
+            this.tb_father.Name = "tb_father";
+            this.tb_father.Size = new System.Drawing.Size(289, 34);
+            this.tb_father.TabIndex = 16;
+            // 
+            // tb_country
+            // 
+            this.tb_country.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_country.Location = new System.Drawing.Point(145, 209);
+            this.tb_country.Name = "tb_country";
+            this.tb_country.Size = new System.Drawing.Size(289, 34);
+            this.tb_country.TabIndex = 15;
+            // 
+            // tb_city
+            // 
+            this.tb_city.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_city.Location = new System.Drawing.Point(145, 249);
+            this.tb_city.Name = "tb_city";
+            this.tb_city.Size = new System.Drawing.Size(289, 34);
+            this.tb_city.TabIndex = 14;
+            // 
+            // tb_number
+            // 
+            this.tb_number.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_number.Location = new System.Drawing.Point(145, 289);
+            this.tb_number.Name = "tb_number";
+            this.tb_number.Size = new System.Drawing.Size(289, 34);
+            this.tb_number.TabIndex = 13;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_name.Location = new System.Drawing.Point(145, 41);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(289, 34);
+            this.tb_name.TabIndex = 12;
+            // 
+            // Female
+            // 
+            this.Female.AutoSize = true;
+            this.Female.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Female.Location = new System.Drawing.Point(271, 423);
+            this.Female.Name = "Female";
+            this.Female.Size = new System.Drawing.Size(99, 32);
+            this.Female.TabIndex = 11;
+            this.Female.TabStop = true;
+            this.Female.Text = "Female";
+            this.Female.UseVisualStyleBackColor = true;
+            // 
+            // Male
+            // 
+            this.Male.AutoSize = true;
+            this.Male.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Male.Location = new System.Drawing.Point(145, 423);
+            this.Male.Name = "Male";
+            this.Male.Size = new System.Drawing.Size(80, 32);
+            this.Male.TabIndex = 10;
+            this.Male.TabStop = true;
+            this.Male.Text = "Male";
+            this.Male.UseVisualStyleBackColor = true;
+            // 
+            // lbl_gender
+            // 
+            this.lbl_gender.AutoSize = true;
+            this.lbl_gender.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_gender.Location = new System.Drawing.Point(17, 425);
+            this.lbl_gender.Name = "lbl_gender";
+            this.lbl_gender.Size = new System.Drawing.Size(80, 28);
+            this.lbl_gender.TabIndex = 9;
+            this.lbl_gender.Text = "Gender:";
+            // 
+            // lbl_born
+            // 
+            this.lbl_born.AutoSize = true;
+            this.lbl_born.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_born.Location = new System.Drawing.Point(17, 362);
+            this.lbl_born.Name = "lbl_born";
+            this.lbl_born.Size = new System.Drawing.Size(122, 28);
+            this.lbl_born.TabIndex = 8;
+            this.lbl_born.Text = "Date of Born";
+            // 
+            // lbl_number
+            // 
+            this.lbl_number.AutoSize = true;
+            this.lbl_number.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_number.Location = new System.Drawing.Point(17, 295);
+            this.lbl_number.Name = "lbl_number";
+            this.lbl_number.Size = new System.Drawing.Size(84, 28);
+            this.lbl_number.TabIndex = 7;
+            this.lbl_number.Text = "Number";
+            // 
+            // lbl_city
+            // 
+            this.lbl_city.AutoSize = true;
+            this.lbl_city.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_city.Location = new System.Drawing.Point(17, 255);
+            this.lbl_city.Name = "lbl_city";
+            this.lbl_city.Size = new System.Drawing.Size(46, 28);
+            this.lbl_city.TabIndex = 6;
+            this.lbl_city.Text = "City";
+            // 
+            // lbl_country
+            // 
+            this.lbl_country.AutoSize = true;
+            this.lbl_country.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_country.Location = new System.Drawing.Point(19, 215);
+            this.lbl_country.Name = "lbl_country";
+            this.lbl_country.Size = new System.Drawing.Size(82, 28);
+            this.lbl_country.TabIndex = 5;
+            this.lbl_country.Text = "Country";
+            // 
+            // lbl_father
+            // 
+            this.lbl_father.AutoSize = true;
+            this.lbl_father.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_father.Location = new System.Drawing.Point(17, 127);
+            this.lbl_father.Name = "lbl_father";
+            this.lbl_father.Size = new System.Drawing.Size(132, 28);
+            this.lbl_father.TabIndex = 4;
+            this.lbl_father.Text = "Father\'s name";
+            // 
+            // lbl_surname
+            // 
+            this.lbl_surname.AutoSize = true;
+            this.lbl_surname.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_surname.Location = new System.Drawing.Point(17, 87);
+            this.lbl_surname.Name = "lbl_surname";
+            this.lbl_surname.Size = new System.Drawing.Size(89, 28);
+            this.lbl_surname.TabIndex = 3;
+            this.lbl_surname.Text = "Surname";
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_name.Location = new System.Drawing.Point(17, 47);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(64, 28);
+            this.lbl_name.TabIndex = 1;
+            this.lbl_name.Text = "Name";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(17, 494);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(417, 45);
+            this.Save.TabIndex = 0;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // tb_filename
+            // 
+            this.tb_filename.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tb_filename.Location = new System.Drawing.Point(136, 12);
+            this.tb_filename.Name = "tb_filename";
+            this.tb_filename.Size = new System.Drawing.Size(150, 29);
+            this.tb_filename.TabIndex = 19;
+            // 
+            // Load
+            // 
+            this.Load.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Load.Location = new System.Drawing.Point(303, 12);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(149, 34);
+            this.Load.TabIndex = 20;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 674);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.tb_filename);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_gender;
+        private System.Windows.Forms.Label lbl_born;
+        private System.Windows.Forms.Label lbl_number;
+        private System.Windows.Forms.Label lbl_city;
+        private System.Windows.Forms.Label lbl_country;
+        private System.Windows.Forms.Label lbl_father;
+        private System.Windows.Forms.Label lbl_surname;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox tb_surname;
+        private System.Windows.Forms.TextBox tb_father;
+        private System.Windows.Forms.TextBox tb_country;
+        private System.Windows.Forms.TextBox tb_city;
+        private System.Windows.Forms.TextBox tb_number;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.RadioButton Female;
+        private System.Windows.Forms.RadioButton Male;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox tb_filename;
+        private System.Windows.Forms.Button Load;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public class Anket{
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Father_name { get; set; }
+            public string Country { get; set; }
+            public string City { get; set; }
+            public string Number { get; set; }
+            public bool Gender { get; set; }
+            public DateTime Date_Born { get; set; }
+            
+        } 
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Anket anket = new Anket();
+            anket.Name = tb_name.Text;
+            anket.Surname = tb_surname.Text;
+            anket.Father_name = tb_father.Text;
+            anket.Country = tb_country.Text;
+            anket.City = tb_city.Text;
+            anket.Number = tb_number.Text;
+            anket.Date_Born = dateTimePicker1.Value;
+
+            if(Male.Checked == true)
+            {
+                anket.Gender = true;
+            }
+            else {
+                anket.Gender = false;
+            }
+            
+            var str = JsonConvert.SerializeObject(anket, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(tb_name.Text + ".json", str);
+
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            Anket anket = new Anket();
+           
+            var jsonStr = File.ReadAllText(tb_filename.Text + ".json");
+            anket = JsonConvert.DeserializeObject<Anket>(jsonStr);
+           
+            tb_name.Text = anket.Name;
+            tb_surname.Text = anket.Surname;
+            tb_father.Text = anket.Father_name;
+            tb_country.Text = anket.Country;
+            tb_city.Text = anket.City;
+            tb_number.Text = anket.Number;
+            dateTimePicker1.Value = anket.Date_Born;
+            if (anket.Gender == true)
+            {
+                Male.Checked = true;
+                Female.Checked = false;
+            }
+            else
+            {
+                Female.Checked = true;
+                Male.Checked = false;
+            }
+        }
+    }
+}
